506cb76d65b466dd6d06ef35d5be7d2184ed5d71
engelwood6 <alper.sensoy@outlook.com>
2021-01-02 01:25:12 +0300

    Gönderme

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplesof3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+            //Find the sum of all the multiples of 3 or 5 below 1000.
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+
+                    sum += i;
+                }
+            }
+                Console.WriteLine(sum);
+                Console.ReadLine();
+            
+        }
+    }
+}
+    
+
